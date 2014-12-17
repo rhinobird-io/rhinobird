92882704dcb18857b20b1df72f25fa24835187fa
Liu Yang <liu_yang@worksap.co.jp>
2014-12-17 19:20:05 +0800

    Add unique index

--- db/migrate/20141217094647_create_plugins.rb
+++ db/migrate/20141217094647_create_plugins.rb
@@ -1,10 +1,11 @@
 class CreatePlugins < ActiveRecord::Migration
   def change
     create_table :plugins do |t|
-      t.string :name, :unique
+      t.string :name
       t.text :description
 
       t.timestamps
     end
+    add_index :plugins, :name, :unique => true
   end
 end
