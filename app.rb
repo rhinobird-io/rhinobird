f4d246c9bc5e8fc78d241bfefea07fa0f342f533
Liu Yang <liu_yang@worksap.co.jp>
2014-12-18 11:55:50 +0800

    Add plugin page.

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/namespace'
 require "sinatra/activerecord"
 #require 'sequel'
 require './config/environments'
@@ -6,37 +7,47 @@ require './models/plugin.rb'
 
 class App < Sinatra::Base
 
-    set :show_exceptions, :after_handler
-    error ActiveRecord::RecordInvalid do
-        status 400
-        body env['sinatra.error'].message
+  register Sinatra::Namespace
+
+  set :show_exceptions, :after_handler
+
+  error ActiveRecord::RecordInvalid do
+    status 400
+    body env['sinatra.error'].message
+  end
+
+  get '/' do
+    "Hello"
+  end
+
+  before do
+    body = request.body.read
+    unless body.empty?
+      @body = JSON.parse(body)
+    end
+  end
+
+  namespace '/developer' do
+
+    get '/?' do
+      send_file File.join(settings.public_folder, 'developer/index.html')
     end
 
-    get '/' do
-        "Hello"
-    end
-
-    before do
-        body = request.body.read
-        unless body.empty?
-            @body = JSON.parse(body)
-        end
-    end
     post '/upload' do
-        p params['file'][:filename]
-        p params['file'][:tempfile]
-        "The file was successfully uploaded!"
+      p params['file'][:filename]
+      p params['file'][:tempfile]
+      "The file was successfully uploaded!"
     end
-
 
     get '/plugins' do
-        Plugin.all.to_json
+      Plugin.all.to_json
     end
 
     post '/plugins' do
-        p "Entering"
-        Plugin.create!(@body)
-        200
+      p "Entering"
+      Plugin.create!(@body)
+      200
     end
+  end
 
 end
