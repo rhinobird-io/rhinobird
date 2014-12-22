498a09dd9b375d623bc6283f43d9eda65c608e1c
Liu Yang <liu_yang@worksap.co.jp>
2014-12-22 15:30:07 +0800

    Implement plugin upload & run logic.

--- models/plugin.rb
+++ models/plugin.rb
@@ -1,3 +1,31 @@
 class Plugin < ActiveRecord::Base
+  require 'zip'
+  require 'fileutils'
+  require 'uuid'
+  PLUGIN_DIR = 'plugins'
+
   validates :name, presence: true, uniqueness: true
+
+  def self.load_from_zip(file)
+    uuid = UUID.new.generate
+    dir = File.join(PLUGIN_DIR, uuid)
+    Zip::File.open(file) { |zip_file|
+      zip_file.each { |f|
+        f_path = File.join(dir, f.name)
+        FileUtils.mkdir_p(File.dirname(f_path))
+        zip_file.extract(f, f_path)
+      }
+    }
+    config = JSON.parse(File.open(File.join(dir, 'plugin.json'), 'r').read)
+    config[:dir_id] = uuid
+    self.new(config)
+  end
+
+  def start
+    dir = File.join(PLUGIN_DIR, @dir_id)
+    pid = spawn("ruby #{File.join(dir, 'app.rb')}", out: "#{File.join(dir, 'out.log')}", err: "#{File.join(dir, 'err.log')}")
+    Process.detach(pid)
+  end
+
+  attr_accessor :dir_id
 end
