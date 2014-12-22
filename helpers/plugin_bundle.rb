549366790197dbe1964f19813510804903285bb4
Liu Yang <liu_yang@worksap.co.jp>
2014-12-22 11:13:57 +0800

    Add uuid

--- helpers/plugin_bundle.rb
+++ helpers/plugin_bundle.rb
@@ -1,13 +1,14 @@
 class PluginBundle
   require 'zip'
   require 'fileutils'
-
+  require 'uuid'
   PLUGIN_DIR = 'plugins'
 
   def self.load_from_zip(file)
+    uuid = UUID.new.generate
     Zip::File.open(file) { |zip_file|
       zip_file.each { |f|
-        f_path = File.join(PLUGIN_DIR, f.name)
+        f_path = File.join(PLUGIN_DIR, uuid, f.name)
         FileUtils.mkdir_p(File.dirname(f_path))
         zip_file.extract(f, f_path)
       }
