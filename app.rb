8814b1eab3c1f253ae903a1d07147eb66f3fcd55
Liu Yang <liu_yang@worksap.co.jp>
2014-12-23 17:33:26 +0800

    Enable `ruby app.rb`

--- app.rb
+++ app.rb
@@ -108,6 +108,9 @@ class App < Sinatra::Base
     post '/users/:userId/dashboard_records' do
       User.find(params[:userId]).dashboard_records.create!(@body)
     end
+
+    # start the server if ruby file executed directly
+    run! if app_file == $0
   end
 
 end
