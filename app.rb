0695ea35b8c09017eb79bc9e432d0e9f3682509c
Liu Yang <liu_yang@worksap.co.jp>
2014-12-23 16:54:57 +0800

    Correct database config and add populate goal for rake

--- app.rb
+++ app.rb
@@ -11,14 +11,6 @@ class App < Sinatra::Base
 
   register Sinatra::ActiveRecordExtension
 
-  configure :development do
-    set :database, {adapter: 'sqlite3', database: 'db/app.sqlite3'}
-  end
-
-  configure :production do
-    set :database, {adapter: 'sqlite3', database: 'db/pro.sqlite3'}
-  end
-
   configure do
     I18n.config.enforce_available_locales = true
   end
