ca331a6a2905806a08f0420d78500f95592b87f5
ding_a <ding_a@worksap.co.jp>
2015-01-05 18:20:09 +0800

    clean front-end integration code

--- models/vote.rb
+++
@@ -1,5 +0,0 @@
-class Vote < ActiveRecord::Base
-  validates :title, uniqueness: true
-  has_many :question
-  has_many :vote_status
-end
--- models/vote_status.rb
+++
@@ -1,5 +0,0 @@
-class VoteStatus < ActiveRecord::Base
-  belongs_to :vote
-
-  validates :user, uniqueness: { scope: [:vote_id], message: "already involved in this vote" }
-end
