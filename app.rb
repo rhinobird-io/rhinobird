070a574cf35288dc8260c0e5daf4310009b34a5e
lijunxiao <li_ju@mail.worksap.com>
2015-01-29 14:31:50 +0800

    Set event's creator when post new event & hide delete button if current user's not the creator.

--- app.rb
+++ app.rb
@@ -386,6 +386,7 @@ class App < Sinatra::Base
     if !event.participants.include? user_self 
       event.participants << user_self
     end
+    event.creator_id = uid
 
     event.save!
     event.to_json(include: {participants: {only: :id}})
