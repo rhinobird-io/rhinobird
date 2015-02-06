37224227ec0543f2141a4ccb8d85f641ba54a940
lijunxiao <li_ju@mail.worksap.com>
2015-02-06 11:51:08 +0800

    Migrate table appointments to associate event with team.

--- db/migrate/20150203040157_update_calendar_event.rb
+++ db/migrate/20150203040157_update_calendar_event.rb
@@ -2,7 +2,7 @@ class UpdateCalendarEvent < ActiveRecord::Migration
   def change
     remove_column :events, :from
     remove_column :events, :to
-    add_column :events, :fromTime, :timestamp
-    add_column :events, :toTime, :timestamp
+    add_column :events, :from_time, :timestamp
+    add_column :events, :to_time, :timestamp
   end
 end
