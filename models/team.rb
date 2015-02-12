74f8f9c8670935b70ad703e740376c6a49e8ea46
lijunxiao <li_ju@mail.worksap.com>
2015-02-12 17:38:35 +0800

    Event notification to team's users & modify event retrieve apis.

--- models/team.rb
+++ models/team.rb
@@ -2,4 +2,7 @@ class Team < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 	has_many :users_teams, dependent: :destroy
   has_many :users, :through => :users_teams
+
+  has_many :team_appointments, foreign_key: :team_participant_id
+  has_many :events, through: :team_appointments
 end
