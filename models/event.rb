3ac11f27de27d778bd7c59ebf3c715f321003ed4
lijunxiao <li_ju@mail.worksap.com>
2015-03-11 11:20:48 +0800

    Issue#183: add repeated number of repeated event to support seperate comment of events that belong to the same repeated event.

--- models/event.rb
+++ models/event.rb
@@ -1,10 +1,8 @@
 class Event < ActiveRecord::Base
-
-
-
   belongs_to :creator, class_name: :User
   has_many :appointments
   has_many :team_appointments
   has_many :participants, through: :appointments
   has_many :team_participants, through: :team_appointments
+  attr_accessor :repeated_number, :integer
 end
