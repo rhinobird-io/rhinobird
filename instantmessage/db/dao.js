7e327ae37b37697ee7a0eda97304d18627f1dd3b
Liu Yang <liu_yang@worksap.co.jp>
2015-04-20 18:39:01 +0800

    Remove im folder

--- instantmessage/db/dao.js
+++
@@ -1,8 +0,0 @@
-var User = require('./models.js').User;
-
-User.findOrCreate({where : { id : 1}}).then(function(users) {
-  users[0].getChannels().then(function(channels){
-    console.log(channels[0]);
-  });
-
-});
