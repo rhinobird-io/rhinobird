d3791022904d73186f45e5a3685e63e61cc969c5
RonaldoLee <ljxnegod@hotmail.com>
2015-06-02 12:23:25 +0800

    Add get_all_parent_teams for team & get_all_teams for user.

--- models/team.rb
+++ models/team.rb
@@ -12,4 +12,21 @@ class Team < ActiveRecord::Base
 
   has_many :parent_teams_relations, foreign_key: 'team_id', class_name: 'TeamsRelation'
   has_many :parent_teams, through: :parent_teams_relations, source: :parent_team
+
+  def get_all_parent_teams
+    teams = []
+    team_ids = {}
+    self.parent_teams.each { |t|
+      team_ids[t.id] = true
+      teams.push(t)
+      un_direct_parents = t.get_all_parent_teams
+      un_direct_parents.each { |p|
+        unless team_ids[p.id]
+          teams.push(p)
+          team_ids[p.id] = true
+        end
+      }
+    }
+    teams
+  end
 end
