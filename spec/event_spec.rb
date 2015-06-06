40bb9751bb2e3bcc7a7e7de65f96146eb370db89
Liu Yang <liuyang1204@hotmail.com>
2015-06-07 02:50:18 +0800

    Add weekly repeat test case & refactor classes

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Event do
   context "with daily repeated event start from #{time}" do
     context 'with frequency 3' do
       context 'with end type Occurrence, repeated times 3' do
-        event = Event.new({repeated: true, repeated_type: 'Daily', from_time: DateTime.parse(time)})
+        event = Daily.new({from_time: DateTime.parse(time)})
         event.repeated_frequency = 3
         event.repeated_end_type = 'Occurrence'
         event.repeated_times = 3
@@ -16,7 +16,7 @@ RSpec.describe Event do
         end
       end
       context 'with end type Date, end date 2015-9-12' do
-        event = Event.new({repeated: true, repeated_type: 'Daily', from_time: DateTime.parse(time)})
+        event = Daily.new({from_time: DateTime.parse(time)})
         event.repeated_frequency = 3
         event.repeated_end_type = 'Date'
         event.repeated_end_date = Date.parse('2015-9-12')
@@ -26,6 +26,25 @@ RSpec.describe Event do
           expect(event.get_next_event(Date.parse('2015-9-12')).from_time).to eq(DateTime.parse('2015-9-12 8:00'))
           expect(event.get_next_event(Date.parse('2015-9-13'))).to be_nil
           expect(event.get_next_event(Date.parse('2015-9-16'))).to be_nil
+        end
+      end
+    end
+  end
+
+  context "with weekly repeated (Tue, Fri) event start from #{Date.parse(time)}" do
+    context 'with frequency 2' do
+      context 'with end type Occurrence, repeated times 3' do
+        event = Weekly.new({from_time: DateTime.parse(time)})
+        event.repeated_frequency = 2
+        event.repeated_end_type = 'Occurrence'
+        event.repeated_times = 2
+        event.repeated_on = '["Tue", "Fri"]'
+        it 'get next event correctly' do
+          expect(event.get_next_event(Date.parse('2015-9-9')).from_time).to eq(DateTime.parse('2015-9-11 8:00'))
+          expect(event.get_next_event(Date.parse('2015-9-11')).from_time).to eq(DateTime.parse('2015-9-11 8:00'))
+          expect(event.get_next_event(Date.parse('2015-9-12')).from_time).to eq(DateTime.parse('2015-9-22 8:00'))
+          expect(event.get_next_event(Date.parse('2015-9-23')).from_time).to eq(DateTime.parse('2015-9-25 8:00'))
+          expect(event.get_next_event(Date.parse('2015-9-26'))).to be_nil
         end
       end
     end
