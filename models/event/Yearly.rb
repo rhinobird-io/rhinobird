dd1ba04aa816abfceabff2235ab2702f3b441212
Liu Yang <liu_yang@worksap.co.jp>
2015-06-08 10:50:49 +0800

    Add yearly

--- models/event/Yearly.rb
+++ models/event/Yearly.rb
@@ -1,4 +1,16 @@
 class Yearly < Repeated
+  def next_occurrence(date)
+    from_time = self.from_time
+    gap = date.year - from_time.year
+    quotient = [gap.fdiv(self.repeated_frequency).ceil, 0].max
+    if gap >= 0 and gap % self.repeated_frequency == 0 and (date.month > from_time.month or (date.month == from_time.month and date.day > from_time.day))
+      quotient += 1
+    end
+    from_time + (quotient * self.repeated_frequency).years
+  end
 
+  def last_occurrence_by_times
+    self.from_time + ((self.repeated_times - 1) * self.repeated_frequency).years
+  end
 
 end
