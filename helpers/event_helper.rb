4f2e4c6e23c19e738134702d6def54a58345fdb2
Liu Yang <liu_yang@worksap.co.jp>
2015-06-08 14:05:33 +0800

    Change priority queue compare function to simplify logic

--- helpers/event_helper.rb
+++ helpers/event_helper.rb
@@ -3,11 +3,11 @@ require 'algorithms'
 module EventHelper
   def self.next_n_events(events, date, num)
     result = []
-    pq = Containers::PriorityQueue.new
+    pq = Containers::PriorityQueue.new{ |x, y| (x <=> y) == -1 }
     events.each do |evt|
       next_event = evt.get_next_event(date)
       unless next_event.nil?
-        pq.push(next_event, -next_event.from_time.to_i)
+        pq.push(next_event, next_event.from_time)
       end
     end
     num.times do
@@ -18,7 +18,7 @@ module EventHelper
         result << evt
         next_event = evt.get_next_event(evt.from_time.to_date + 1)
         unless next_event.nil?
-          pq.push(next_event, -next_event.from_time.to_i)
+          pq.push(next_event, next_event.from_time)
         end
       end
     end
