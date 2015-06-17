5d2ddd3b6eba2977f8612b3f16b9d611b43be75c
RonaldoLee <ljxnegod@hotmail.com>
2015-06-17 13:36:20 +0800

    Add default redis url for production mode.

--- app.rb
+++ app.rb
@@ -93,7 +93,7 @@ class App < Sinatra::Base
       end
     end
 
-    redis_url = ENV['REDISCLOUD_URL'] || ENV['OPENREDIS_URL'] || ENV['REDISGREEN_URL'] || ENV['REDISTOGO_URL']
+    redis_url = ENV['REDISCLOUD_URL'] || ENV['OPENREDIS_URL'] || ENV['REDISGREEN_URL'] || ENV['REDISTOGO_URL'] || 'redis://localhost:6379'
     uri = URI.parse(redis_url)
     Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
     Resque.redis.namespace = 'resque:rhinobird'
