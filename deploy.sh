81c996b5d80667f37b6aee5e07bb2b785836178a
RonaldoLee <ljxnegod@hotmail.com>
2015-07-21 18:41:39 +0800

    Modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ mv public/_assets/main.js public/_assets/main$filename.js
 mv public/_assets/main.css public/_assets/main$filename.css
 
 echo "development:
-	hostname: localhost:8000
-	script_url: 'http://localhost:2992/_assets/main$filename.js'
-	css_url: ''
-	
+  hostname: localhost:8000
+  script_url: 'http://localhost:2992/_assets/main$filename.js'
+  css_url: ''	
+
 production:
-	hostname: rhinobird.workslan
-	script_url: '/platform/_assets/main$filename.js'
-	css_url: '/platform/_assets/main$filename.css'" > "config/platform.yml"
+  hostname: rhinobird.workslan
+  script_url: '/platform/_assets/main$filename.js'
+  css_url: '/platform/_assets/main$filename.css'" > "config/platform.yml"
 
 git add config/platform.yml
 git add public
