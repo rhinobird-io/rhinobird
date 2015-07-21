123a60165dae262ec980815714687b15847a6cc1
RonaldoLee <ljxnegod@hotmail.com>
2015-07-21 18:21:46 +0800

    Change deploy file.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,20 @@
 git co -B deploy
+
+filename=$(date +'%s')
+mv public/_assets/main.js public/_assets/main$filename.js
+mv public/_assets/main.css public/_assets/main$filename.css
+
+echo "development:
+	hostname: localhost:8000
+	script_url: 'http://localhost:2992/_assets/main$filename.js'
+	css_url: ''
+	
+production:
+	hostname: rhinobird.workslan
+	script_url: '/platform/_assets/main$filename.js'
+	css_url: '/platform/_assets/main$filename.css'"
+
+git add config/platform.yml
 git add public
 git commit -m 'add assets'
 git push deis deploy:master -f
