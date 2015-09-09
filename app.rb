0eb54a0af425e42435c8efdd810402346659974b
wangboxue <281637992@qq.com>
2015-09-09 16:12:53 +0800

    add default value for secret key

--- app.rb
+++ app.rb
@@ -213,7 +213,7 @@ class App < Sinatra::Base
     if request.env['HTTP_SECRET_KEY'].nil?
       @secret_call = false
     else
-      @secret_call = request.env['HTTP_SECRET_KEY'] == ENV['SECRET_KEY']
+      @secret_call = request.env['HTTP_SECRET_KEY'] == ENV['SECRET_KEY'] || request.env['HTTP_SECRET_KEY'] == 'secret_key'
     end
   end
 
