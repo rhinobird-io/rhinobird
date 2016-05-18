bc85c9b9b70e4763cda5e0564e1ca3b3889293a2
wangboxue <281637992@qq.com>
2016-05-18 17:41:07 +0800

    modify config file by replacing keyword

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,12 @@ filename=$(date +'%s')
 mv public/_assets/main.js public/_assets/main$filename.js
 mv public/_assets/main.css public/_assets/main$filename.css
 
-echo "development:
-  hostname: localhost:8000
-  script_url: 'http://localhost:2992/_assets/main$filename.js'
-  css_url: ''
-  email: 'rhinobird.worksap.test@gmail.com'
 
-
-production:
-  hostname: rhinobird.workslan
-  script_url: '/platform/_assets/main$filename.js'
-  css_url: '/platform/_assets/main$filename.css'
-  email: 'rhinobird.worksap@gmail.com'" > "config/platform.yml"
+content=$(<"config/platform.yml")
+from="_assets/main."
+to="_assets/main$filename."
+formated=${content//$from/$to}
+echo "$formated" > "config/platform.yml"
 
 
 git add config/platform.yml
