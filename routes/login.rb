e2587d533356f55465448768b40da887e019df66
An Xiaoqiang <an_x@worksap.co.jp>
2017-05-23 16:52:55 +0800

    introducing member center, use ticket

--- routes/login.rb
+++ routes/login.rb
@@ -20,37 +20,8 @@ class App < Sinatra::Base
       end
     end
 
-
     post '/logout' do
       #TODO
-    end
-
-    get '/mc_login_call' do
-      if verify_sign(params[:ticket], params[:sign])
-        response = HTTParty.post('http://mcenter.internal.worksap.com/auth', body: {app_id: 'rhinobird', ticket: params[:ticket], sign: auth_sign(params[:ticket])}).parsed_response
-        email = reponse["email_prefix"] + "@worksap.co.jp"
-        user = User.find_by(email: email)
-        if user.nil?
-          status 410
-        elsif user.password == @body["password"]
-          token = SecureRandom.hex
-          RestClient.post auth_url, {'token' => token, 'userId' => user.id.to_s}.to_json, :content_type => :json
-          response.set_cookie("Auth", {
-              :value => token,
-              :httponly => false,
-              :path => '/'
-          })
-          user.to_json(:except => :encrypted_password)
-        else
-          status 401
-        end
-      else
-        status 406
-      end
-    end
-
-    get 'login_from_member_center' do
-      redirect 'http://mcenter.internal.worksap.com/login?app_id=rhinobird'
     end
 
     get '/login' do
@@ -66,15 +37,5 @@ class App < Sinatra::Base
         Team.find_by_name('Guest').users << user
         user.to_json(:except => :encrypted_password)
     end
-  end
-
-
-  def verify_sign(ticket, sign)
-    sign == DIgest::MD5.hexdigest("#{ticket}-#{ENV['RHINOBIRD_CRENDENTIAL']}")
-  end
-
-
-  def auth_sign(ticket)
-    Digest::MD5.hexdigest("rhinobird-#{ticket}-#{ENV['RHINOBIRD_CREDENTIAL']}")
   end
 end
